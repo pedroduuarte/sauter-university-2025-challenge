30177ac29ad30f03fc7be9de8ee58644322317ac
L3onVictor <leonardolv1112@gmail.com>
2025-09-25 00:47:58 -0300

    feat: Implementado o agente que busca dados da ONS

--- src/adk_agents/multi_tool_agent/agent.py
+++ src/adk_agents/multi_tool_agent/agent.py
@@ -1,19 +1,41 @@
+from dotenv import load_dotenv
+import os
 from google.adk.agents import Agent
 from google.adk.tools import google_search
-from google.adk.tools import bigquery
+from google.adk.tools.bigquery import BigQueryToolset, BigQueryCredentialsConfig
+from google.adk.tools.bigquery.config import BigQueryToolConfig, WriteMode
+import google.auth
 from google.adk.agents import LlmAgent
 from google.adk.tools import agent_tool
 
+application_default_credentials, _ = google.auth.default()
+credentials_config = BigQueryCredentialsConfig(
+  credentials=application_default_credentials
+)
+
+load_dotenv()
+
+PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
+DATASET = os.getenv("BIGQUERY_DATASET")
+
+# configurção do BigQueryToolset
+tool_config = BigQueryToolConfig(write_mode=WriteMode.BLOCKED)
+
+bigquery_toolset = BigQueryToolset(
+    credentials_config=credentials_config, bigquery_tool_config=tool_config
+)
+
+# Agente para responder perguntas sobre o site Sauter Digital
 sauter_agent = Agent(
     name="sauter_info_agent",
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     description=(
         "Agente para responder perguntas sobre o site Sauter Digital."
     ),
     instruction=(
         """
         Você é um agente prestativo que responde perguntas sobre a empresa Sauter Digital.
-        Use a ferramenta google_search para encontrar informações no site da empresa, garantindo que toda pesquisa inclua o filtro 'site:sauter.digital' para que os resultados sejam apenas desse domínio."
+        Use a ferramenta google_search para encontrar informações no site da empresa, garantindo que toda pesquisa inclua o filtro 'site:https://sauter.digital/' para que os resultados sejam apenas desse domínio."
         """
     ),
     tools=[google_search],
@@ -22,19 +44,22 @@ sauter_agent = Agent(
 # Agente para consultar dados no BigQuery
 bigquery_agent = Agent(
     name="bigquery_agent",
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     description=(
         "Agente para buscar e responder perguntas usando dados do Google BigQuery."
     ),
     instruction=(
-        """
-        Você é um agente especializado em consultar dados do BigQuery.
-        No momento você é apenas um prototipo e não tem acesso a dados reais.
-        Caso seja feito alguma pergunta, responda com dados ficticios.
-        
+        f"""
+        Você é um analista de dados especializado em consultar dados do Google BigQuery.
+        Ao receber uma solicitação, identifique a necessidade do usuário, formule e execute queries SQL no BigQuery usando a ferramenta disponível.
+        Utilizeo projeto onde o ID é '{PROJECT}' o dataset '{DATASET}' para todas as consultas.
+        A tabela que será utilizada é a 'trusted_data'.
+        Retorne os resultados de forma clara e objetiva, explicando os dados quando necessário.
+        Se a consulta for complexa, explique os passos e forneça insights relevantes com base nos dados retornados.
+        Caso não tenha informações suficientes para responder, peça detalhes adicionais ao usuário.
         """
     ),
-    tools=[],
+    tools=[bigquery_toolset],
 )
 
 # Agente orquestrador
